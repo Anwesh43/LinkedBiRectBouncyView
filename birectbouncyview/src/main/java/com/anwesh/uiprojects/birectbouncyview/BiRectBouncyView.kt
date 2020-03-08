a195f0c726445212bb539f8ce780c8b96b8c3833
Anwesh43 <anweshthecool0@gmail.com>
2020-03-08 10:38:24 +0530

    adding Canvas extensions method

--- birectbouncyview/src/main/java/com/anwesh/uiprojects/birectbouncyview/BiRectBouncyView.kt
+++ birectbouncyview/src/main/java/com/anwesh/uiprojects/birectbouncyview/BiRectBouncyView.kt
@@ -24,3 +24,38 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawBiRectBouncyLine(i : Int, scale : Float, w : Float, paint : Paint) {
+    val gap : Float = w / (colors.size)
+    val sf : Float = scale.sinify()
+    val sfi : Float = sf.divideScale(i, parts)
+    save()
+    translate(gap * (i + 1), 0f)
+    for (j in 0..(lines)) {
+        val sfij : Float = sfi.divideScale(i, lines)
+        save()
+        translate(gap * j, 0f)
+        rotate(-90f * j)
+        drawLine(0f, 0f, gap * sfij, 0f, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawBiRectBouncyLines(scale : Float, w : Float, paint : Paint) {
+    for (j in 0..(parts - 1)) {
+        drawBiRectBouncyLine(j, scale, w, paint)
+    }
+}
+
+fun Canvas.drawBRBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    save()
+    translate(0f, h / 2)
+    drawBiRectBouncyLines(scale, w, paint)
+    restore()
+}
