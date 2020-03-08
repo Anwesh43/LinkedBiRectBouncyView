f928636e4d90c05592fb91d141e42a262c097c1e
Anwesh43 <anweshthecool0@gmail.com>
2020-03-08 10:55:32 +0530

    adding Renderer class

--- birectbouncyview/src/main/java/com/anwesh/uiprojects/birectbouncyview/BiRectBouncyView.kt
+++ birectbouncyview/src/main/java/com/anwesh/uiprojects/birectbouncyview/BiRectBouncyView.kt
@@ -186,4 +186,27 @@ class BiRectBouncyView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : BiRectBouncyView) {
+
+        private val animator : Animator = Animator(view)
+        private val brb : BiRectBouncyLine = BiRectBouncyLine(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            brb.draw(canvas, paint)
+            animator.animate {
+                brb.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            brb.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
