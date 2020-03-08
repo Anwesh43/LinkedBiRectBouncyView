44fbcceb5970423a7bfae502430f1038cba08eca
Anwesh43 <anweshthecool0@gmail.com>
2020-03-08 10:40:09 +0530

    adding BiRectBouncyView class

--- birectbouncyview/src/main/java/com/anwesh/uiprojects/birectbouncyview/BiRectBouncyView.kt
+++ birectbouncyview/src/main/java/com/anwesh/uiprojects/birectbouncyview/BiRectBouncyView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawBRBNode(i : Int, scale : Float, paint : Paint) {
     drawBiRectBouncyLines(scale, w, paint)
     restore()
 }
+
+class BiRectBouncyView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
